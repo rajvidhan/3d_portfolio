2586c1a3a8997fc206b1883f8d0b193ec102702e
vidhan gour <vidhangour20@gmail.com>
2023-12-30 10:26:01 -0800

    sf

--- src/constants/index.js
+++ src/constants/index.js
@@ -31,8 +31,12 @@ import {
       title: "About",
     },
     {
-      id: "work",
-      title: "Work",
+      id: "experience",
+      title: "Experience",
+    },
+    {
+      id: "project",
+      title: "Project",
     },
     {
       id: "contact",
